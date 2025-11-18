5a74e22e1920372e6bc598072f5caf9dd5c95063
anitha <chittiboinaanitha@gmail.com>
2025-11-18 10:58:49 +0530

    Applied some changes in table

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { MaterialReactTable } from "material-react-table";
 import axios from "axios";
-
+import { Paper,Box } from "@mui/material";
 const CompaniesTable = () => {
   const [companies, setCompanies] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -22,13 +22,32 @@ const CompaniesTable = () => {
   }, []);
 
   const columns = [
-    { accessorKey: "id", header: "ID" },
-    { accessorKey: "name", header: "Name" },
-    { accessorKey: "location", header: "Location" },
-    { accessorKey: "industry", header: "Industry" },
+    { accessorKey: "id", header: "ID" , },
+    { accessorKey: "name", header: "Name" ,},
+    { accessorKey: "location", header: "Location"},
+    { accessorKey: "industry", header: "Industry",},
   ];
 
   return (
+     <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh" // centers vertically
+      bgcolor="#b6d3adff"
+      //padding={2}
+      minWidth={1500}
+    >
+    <Paper
+        elevation={3}
+        style={{
+          padding: "16px",
+          width: "80%",
+          maxWidth: "900px",
+          border: "2px solid #181a1bff", // blue border
+          borderRadius: "12px",        // rounded corners
+        }}
+      >
     <MaterialReactTable
       columns={columns}
       data={companies}
@@ -38,8 +57,11 @@ const CompaniesTable = () => {
       enableSorting={true}
       enablePagination={true}
       initialState={{ showGlobalFilter: true }}
+      minWidth={1000}
 
     />
+    </Paper>
+    </Box>
   );
 };
 
